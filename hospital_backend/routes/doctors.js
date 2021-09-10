6d15753f80e115df06d79b786c0fc94cda4f4ff3
lakineth <lakindu.nethmi@gmail.com>
2021-09-10 23:39:49 +0530

    my project

---
+++ hospital_backend/routes/doctors.js
@@ -0,0 +1,91 @@
+const router = require("express").Router();
+let Doctor = require("../models/Doctor.js");
+
+router.route("/add").post((req,res) =>{
+
+    const did = Number(req.body.did);
+    const name = req.body.name;
+    const contact = Number(req.body.contact);
+    const email = req. body.email;
+    const gender = req.body. gender;
+    const speciality = req.body.speciality;
+
+    const newDoctor = new Doctor({
+        did,
+        name,
+        contact,
+        email,
+        gender,
+        speciality
+    })
+
+    newDoctor.save().then(()=>{
+       
+        res.json("Doctor Added")
+    }).catch((err)=>{
+
+        console.log(err);
+    })
+})
+
+router.route("/").get((req,res)=>{
+
+    Doctor.find().then((doctors)=>{
+        res.json(doctors)
+    }).catch((err)=>{
+        console.log(err)
+    })
+})
+
+router.route("/update/:did").put(async(req,res)=>{
+
+    let docID = req.params.did;
+    const{ name, contact, email, gender, speciality} = req.body;
+
+    const updateDoctor = {
+        name,
+        contact,
+        email,
+        gender,
+        speciality
+    }
+
+    const update = await Doctor.findOneAndUpdate(docID, updateDoctor).then(()=> {
+
+        res.status(200).send({status:"Doctor updated" })
+    }).catch((err)=> {
+        res.status(500).send({status: "Error with updating data", error: err.message});
+    })
+
+})
+
+router.route("/delete/:email").delete(async(req,res)=> {
+
+    const docID = req.params.email;
+
+    await Doctor.findOneAndDelete(docID).then(()=> {
+        res.status(200).send({status: "Doctor was deleted"});
+    }).catch((err)=> {
+       console.log(err.message);
+       res.status(500).send({status: "Error with delete doctor", error: err.message});
+    })
+})
+
+router.route("/get/:id").get(async(req,res)=> {
+
+    let docID = req.params.id;
+
+   const doctor = await Doctor.findById(docID).then((Doctor)=> {
+        res.status(200).send({status: "Doctor Fetched", Doctor})
+    }).catch((err)=> {
+        console.log(err.message);
+        res.status(500).send({status: "error with get doctor", error: err.message});  
+     })
+
+})
+
+
+
+
+
+module.exports = router;
